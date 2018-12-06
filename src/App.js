0342f93ec5eb6cdc6fc812de10067934180ffbf1
shubham jain <shubhamjain2908@gmail.com>
2018-12-06 17:13:53 +0530

    manipulating state and passing data from on component to other 42

--- src/App.js
+++ src/App.js
@@ -1,27 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Shubham', age: 22 },
+      { name: 'Max', age: 28 },
+      { name: 'Steph', age: 26 }
+    ]
+  }
+
+switchNameHandler = (newName) => {
+  // console.log('Was clicked!')
+  // DON'T DO THIS this.state.persons[0].name = 'Shubham Jain';
+  this.setState({persons: [
+    { name: newName, age: 22 },
+    { name: 'Maxi', age: 29 },
+    { name: 'Steph', age: 69 }
+  ]})
+}
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Hi, I'm a React App.</h1>
+        <p>This is really working</p>
+        <button onClick={this.switchNameHandler.bind(this, 'Shubham')}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age = {this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age = {this.state.persons[1].age} click={this.switchNameHandler.bind(this, 'MAx!')}>My Hobbies: Racing</Person>
+        <Person name={this.state.persons[2].name} age = {this.state.persons[2].age}/>
       </div>
     );
+    // return React.createElement('div', null, React.createElement('h1', {className: 'App'}, 'Hi, I\'m a React App.'));
   }
 }
 
